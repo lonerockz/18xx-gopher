95c15604fcac03ba6662d811d297f1c6c6c2c2cd
lonerockz <smccaa@gmail.com>
2019-12-18 10:22:36 -0800

    base setup with views

---
+++ src/components/companies/CompanyList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        company list
+        <ul>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/players/Playerlist.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        playerlist
+<ul>
+    <li></li>
+</ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
